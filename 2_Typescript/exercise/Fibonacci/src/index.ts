8bc0ba7fa4a6034bc3bb340162419ba2e37b60e6
hungnv612 <hungdaubasg@gmail.com>
2022-08-12 16:09:48 +0700

    first commit

---
+++ 2_Typescript/exercise/Fibonacci/src/index.ts
@@ -0,0 +1,18 @@
+function fibonacciNumber(n: number): number {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return fibonacciNumber(n - 2) + fibonacciNumber(n - 1);
+    }
+}
+
+let sum = 0;
+
+console.log("10 số fibonacci đầu tiên là:");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(fibonacciNumber(i))
+    sum += fibonacciNumber(i);
+}
+
+console.log("Tổng 10 số fibonacci đầu tiên: " + sum);
---
+++ 2_Typescript/practice/DeclareAndVariables/src/index.ts
@@ -0,0 +1,8 @@
+// happy coding 👻
+console.log("hello world");
+let width: number;
+let height: number;
+width = 10.5;
+height = 20;
+let area: number = width*height;
+alert(`Diện tích hình chữ nhật: ${area}`);
